8050185810d95995315536aef58b5a5dbb310b65
Luis Estigarribia <luisalfredoestigarribiasosa85@gmail.com>
2026-01-01 00:41:28 -0300

    stage and commit changes before pull

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :articles do
+<<<<<<< HEAD
     resources :comments, shallow: true
+=======
+    resources :comments, only: [:create]
+>>>>>>> 98755e4 (stage and commit changes before pull)
   end
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'home#index'
